6fad01a087fe62523d06dd574d59fda6ad0dd7f7
shuchao <645904877@qq.com>
2017-10-09 20:50:06 +0800

    debug

--- cmd/cobra.go
+++ cmd/cobra.go
@@ -7,50 +7,70 @@ import (
 )
 
 func usageTemplate() string {
-	return `Usage:{{if .Runnable}}{{if .HasAvailableFlags}}
-  {{appendIfNotPresent .UseLine "[OPTIONS] COMMAND [arg...]"}}{{else}}{{.UseLine}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-  {{ .CommandPath}} [command]
-  {{end}}{{if gt .Aliases 0}}
-Aliases:{{.NameAndAliases}}
-{{end}}{{if .HasExample}}
-Examples:{{ .Example }}
-{{end}}{{ if .HasAvailableLocalFlags}}
+	//	return `Usage:{{if .Runnable}}{{if .HasAvailableFlags}}
+	//  {{appendIfNotPresent .UseLine "[OPTIONS] COMMAND [arg...]"}}{{else}}{{.UseLine}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+	//  {{ .CommandPath}} [command]
+	//  {{end}}{{if gt .Aliases 0}}
+	//Aliases:{{.NameAndAliases}}
+	//{{end}}{{if .HasExample}}
+	//Examples:{{ .Example }}
+	//{{end}}{{ if .HasAvailableLocalFlags}}
+	//Options:
+	//{{.LocalFlags.FlagUsages | trimRightSpace}}
+	//{{end}}{{ if .HasAvailableSubCommands}}
+	//Commands:{{range .Commands}}{{if .IsAvailableCommand}}
+	//  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}
+	//{{end}}{{ if .HasAvailableInheritedFlags}}
+	//Global Flags:
+	//{{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
+	//Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
+	//  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
+	//Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+	//`
+	return `Usage:	{{if not .HasSubCommands}}{{.UseLine}}{{end}}{{if .HasSubCommands}}{{ .CommandPath}} COMMAND{{end}}
+
+{{ .Short | trim }}{{if gt .Aliases 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{ .Example }}{{end}}{{if .HasFlags}}
+
 Options:
-{{.LocalFlags.FlagUsages | trimRightSpace}}
-{{end}}{{ if .HasAvailableSubCommands}}
+{{.Flags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableSubCommands}}
+
 Commands:{{range .Commands}}{{if .IsAvailableCommand}}
-  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}
-{{end}}{{ if .HasAvailableInheritedFlags}}
-Global Flags:
-{{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
-Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
-  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasSubCommands }}
+
+Run '{{.CommandPath}} COMMAND --help' for more information on a command.{{end}}
 `
 }
 
 func helpCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:               "help [command]",
-		Short:             "Help about the command",
-		PersistentPreRun:  func(cmd *cobra.Command, args []string) {},
-		PersistentPostRun: func(cmd *cobra.Command, args []string) {},
+		Use:   "help [command]",
+		Short: "Help about the command",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			// todo
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			// todo
+		},
 		RunE: func(c *cobra.Command, args []string) error {
 			cmd, args, e := c.Root().Find(args)
 			if cmd == nil || e != nil || len(args) > 0 {
 				return fmt.Errorf("unknown help topic: %v", strings.Join(args, " "))
 			}
 
-			helpFunc := cmd.HelpFunc()
-			helpFunc(cmd, args)
+			cmd.HelpFunc()(cmd, args)
 			return nil
 		},
 	}
 }
 
 func helpTemplate() string {
-	return `
-{{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}`
+	return "\n{{if or .Runnable .HasSubCommands}}{{.UsageString}}{{end}}"
 }
 
 func FlagErrorFunc(cmd *cobra.Command, err error) error {
